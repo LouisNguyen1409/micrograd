4eb10b48e236203f3203182de3701b4aa2a377e7
Louis Nguyen <ndmn004@gmail.com>
2025-02-07 15:15:21 +1100

    Fix gradient accumulation in micrograd's Value class

--- micrograd.py
+++ micrograd.py
@@ -18,8 +18,8 @@ class Value:
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
         out._backward = _backward
         return out
@@ -28,8 +28,8 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
 
         out._backward = _backward
         return out
