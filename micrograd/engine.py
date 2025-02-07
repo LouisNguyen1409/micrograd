66a485cccc50795ebc20d9cfcdbdde43265a5bd0
Louis Nguyen <ndmn004@gmail.com>
2025-02-07 16:01:31 +1100

    Add support for additional arithmetic operations in Value class

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -15,6 +15,7 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -25,6 +26,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -34,6 +36,19 @@ class Value:
         out._backward = _backward
         return out
 
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __truediv__(self, other):  # self / other
+        return self * other**-1
+
     def tanh(self):
         n = self.data
         t = (math.exp(2*n) - 1) / (math.exp(2*n) + 1)
@@ -41,6 +56,16 @@ class Value:
 
         def _backward():
             self.grad = (1 - t**2) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
 
         out._backward = _backward
         return out
@@ -60,3 +85,18 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
+
+    def __radd__(self, other):  # other + self
+        return self + other
+
+    def __rmul__(self, other):  # other * self
+        return self * other
+
+    def __rtruediv__(self, other):  # other / self
+        return other * self**-1
+
+    def __sub__(self, other):  # self - other
+        return self + (-other)
+
+    def __rsub__(self, other):  # other - self
+        return other + (-self)
